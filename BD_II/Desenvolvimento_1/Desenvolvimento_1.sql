7cc8056aa48712cf877b26e319ba08c6bd53d438
Adler_Neto <adler.neto@outlook.com.br>
2024-08-25 19:01:16 -0300

    atualizando exercico BD_II - Desencolvimento_1

--- BD_II/Desenvolvimento_1/Desenvolvimento_1.sql
+++ BD_II/Desenvolvimento_1/Desenvolvimento_1.sql
@@ -1,3 +1,10 @@
+-- Desenvolva um banco de dados e relacione tabelas através de chaves estrangeiras ou nomes de colunas iguais. Siga as instruções:
+-- crie uma base de dados; 
+-- crie tabelas nessa base de dados;
+-- em cada tabela, adicione atributos;
+-- insira dados em cada tabela;
+-- utilize os comandos Joins para realizar consultas nas tabelas. 
+
 -- Criando o BD
 CREATE DATABASE CadastroClientes;
 USE CadastroClientes;
